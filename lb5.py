1085ce4efc8c3cedd5744171e42801e5b851ee96
NazarGonchar <90674290+NazarGonchar@users.noreply.github.com>
2022-11-20 18:30:40 +0200

    Add files via upload

---
+++ lb5.py
@@ -0,0 +1,25 @@
+import numpy as np 
+from scipy import optimize 
+from scipy.misc import derivative 
+import math 
+x0 = 1.8 
+y0 = 0.6 
+def f1(y): 
+    return -1*math.sin(y + 1) + 0.8 
+def f2 (x): 
+    return math.sin(x - 1) + 1.3  
+def iter (x,y,e): 
+    xn = x 
+    yn = y 
+    xn1 = f2(x) 
+    yn1 = f1(y) 
+    n = 1 
+    while ((abs(xn1-xn)>=e) & (abs(yn1-yn) >=e)): 
+        xn = xn1 
+        yn = yn1 
+        xn1 = f2(yn) 
+        yn1 = f1(xn) 
+        n += 1 
+    print ('Simple iteration:') 
+    print ('x=', xn, '\ny=',yn,'\nThe amount of iteration = ',n) 
+iter(x0,y0,0.0001) 
